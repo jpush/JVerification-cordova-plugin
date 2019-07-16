b10f046d89a7e9f88950b0891a7ddcd5ba9f192f
weiry <weiry@jpush.cn>
2019-07-16 16:54:36 +0800

    ios 插件代码  和 README.md

--- src/ios/JVerificationPlugin.m
+++ src/ios/JVerificationPlugin.m
@@ -12,6 +12,9 @@
 // 如果需要使用 idfa 功能所需要引入的头文件（可选）
 #import <AdSupport/AdSupport.h>
 
+#define UIColorFromRGBValue(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+
 @implementation JVerificationPlugin
 
 
@@ -19,7 +22,7 @@
 {
     NSString *plistPath = [[NSBundle mainBundle]pathForResource:@"JVerificationConfig" ofType:@"plist"];
     NSMutableDictionary *dataDic = [[NSMutableDictionary alloc]initWithContentsOfFile:plistPath];
-    NSLog(@"%@",dataDic);//直接打印数据
+//    NSLog(@"%@",dataDic);//直接打印数据
     
     // 如需使用 IDFA 功能请添加此代码并在初始化配置类中设置 advertisingId
     NSString *idfaStr = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
@@ -64,7 +67,7 @@
         //        {"code":2000,"content":"ok","operator":"CM"}
 //        result 字典 获取到token时key有operator、code、token字段，获取不到token时key为code和content字段
 //        NSLog(@"getToken result:%d", [timeout intValue]);
-        NSLog(@"getToken result:%@", result);
+//        NSLog(@"getToken result:%@", result);
         
         NSNumber* code = result[@"code"];
         NSString* token = result[@"token"];
@@ -101,7 +104,7 @@
     [JVERIFICATIONService verifyNumber:entity result:^(NSDictionary *result) {
 //        {"code":1000,"content":"ok","operator":"CM"}
 //        result 字典 key为code和content两个字段
-        NSLog(@"verifyNumber result:%@", result);
+//        NSLog(@"verifyNumber result:%@", result);
         
         
         NSNumber* code = result[@"code"];
@@ -143,7 +146,7 @@
         //        {"code":7000,"content":"ok"}
         //       result 字典 key为code和message两个字段
 //        NSLog(@"preLogin result:%d", [timeout intValue]);
-        NSLog(@"preLogin result:%@", result);
+//        NSLog(@"preLogin result:%@", result);
         
         
         NSNumber* code = result[@"code"];
@@ -190,7 +193,7 @@
 //        {"code":6000,"content":"ok","operator":"CM"}
 //        result 字典 获取到token时key有operator、code、loginToken字段，获取不到token是key为code和content字段
 //        NSLog(@"getAuthorizationWithController result:%d", b);
-        NSLog(@"getAuthorizationWithController result:%@", result);
+//        NSLog(@"getAuthorizationWithController result:%@", result);
         
         
         NSNumber* code = result[@"code"];
@@ -241,12 +244,12 @@
 {
     
     NSString* json = [command.arguments objectAtIndex:0];
-    NSLog(@"setCustomUIWithConfig:%@", json);
+//    NSLog(@"setCustomUIWithConfig:%@", json);
     
     NSData *data = [json dataUsingEncoding:NSUTF8StringEncoding];
     NSDictionary *dict = APNativeJSONObject(data);
     
-    NSLog(@"setCustomUIWithConfig dict:%@", dict);
+    
     
     
     /*移动*/
@@ -258,46 +261,100 @@
     
     NSArray* arrayKeys = [dict allKeys];
     for(NSString * key in arrayKeys){
-        
+        setJVUIConfig(key,dict,mobileUIConfig);
+        setJVUIConfig(key,dict,unicomUIConfig);
+        setJVUIConfig(key,dict,telecomUIConfig);
     }
-//    CDVPluginResult* pluginResult = nil;
-//    if (myarg != nil) {
-//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-//    } else {
-//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
-//    }
-//    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [JVERIFICATIONService customUIWithConfig:mobileUIConfig];
+    [JVERIFICATIONService customUIWithConfig:unicomUIConfig];
+    [JVERIFICATIONService customUIWithConfig:telecomUIConfig];
 }
 
 void setJVUIConfig(NSString* key ,NSDictionary *dict,
-                   JVMobileUIConfig *mobileUIConfig ,JVUnicomUIConfig *unicomUIConfig,JVTelecomUIConfig *telecomUIConfig  ){
+                   JVUIConfig *jvUIConfig){
     
-    
-    if ([key containsString:@""]) {
-    }else if([key containsString:@""]){
+    if ([key containsString:@"authPageBackgroundImage"]) {
+        jvUIConfig.authPageBackgroundImage = [UIImage imageNamed:dict[key]];
+    }else if([key containsString:@"navColor"]){
+        jvUIConfig.navColor = UIColorFromRGBValue([dict[key] intValue]);
+//        NSLog(@"jvUIConfig.navColor :%@", jvUIConfig.navColor );
+    }else if([key containsString:@"barStyle"]){
+        jvUIConfig.barStyle = [dict[key] intValue];//UIBarStyle;
+        //NSLog(@"jvUIConfig.barStyle :%ld", jvUIConfig.barStyle);
+    }else if([key containsString:@"navText"]){
+//        jvUIConfig.navText= [[NSAttributedString alloc] initWithString:dict[key]];//NSAttributedString ;
         
-    }else if([key containsString:@""]){
+        NSArray* textArry = dict[key];
+        NSMutableAttributedString *navText = [[NSMutableAttributedString alloc] initWithString:textArry[0]];
+//        [navText addAttribute:NSFontAttributeName
+//                         value:[UIFont boldSystemFontOfSize:[textArry[1] floatValue]]
+//                         range:[textArry[0] rangeOfString:textArry[0]]];
+        [navText addAttribute:NSForegroundColorAttributeName
+                          value:UIColorFromRGBValue([textArry[1] intValue])
+                          range:[textArry[0] rangeOfString:textArry[0]]];
         
-    }else if([key containsString:@""]){
+        jvUIConfig.navText = navText;
         
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
-    }else if([key containsString:@""]){
-        
+//        NSLog(@"jvUIConfig.navText :%@", jvUIConfig.navText);
+    }else if([key containsString:@"navReturnImg"]){
+        jvUIConfig.navReturnImg = [UIImage imageNamed:dict[key]];
+    }else if([key containsString:@"navControl"]){
+//        jvUIConfig.navControl = UIBarButtonItem;
+    }else if([key containsString:@"navCustom"]){
+        jvUIConfig.navCustom = [dict[key] boolValue];//BOOL
+    }else if([key containsString:@"logoImg"]){
+        jvUIConfig.logoImg = [UIImage imageNamed:dict[key]];
+    }else if([key containsString:@"logoWidth"]){
+        jvUIConfig.logoWidth =  [dict[key] floatValue];//CGFloat ;
+    }else if([key containsString:@"logoHeight"]){
+        jvUIConfig.logoHeight =[dict[key] floatValue];//CGFloat;
+    }else if([key containsString:@"logoOffsetY"]){
+        jvUIConfig.logoOffsetY =[dict[key] floatValue];//CGFloat;
+    }else if([key containsString:@"logoHidden"]){
+        jvUIConfig.logoHidden =[dict[key] boolValue];//BOOL
+    }else if([key containsString:@"logBtnText"]){
+        jvUIConfig.logBtnText = dict[key];
+    }else if([key containsString:@"logBtnOffsetY"]){
+        jvUIConfig.logBtnOffsetY =[dict[key] floatValue];//CGFloat;
+    }else if([key containsString:@"logBtnTextColor"]){
+        jvUIConfig.logBtnTextColor = UIColorFromRGBValue([dict[key] intValue]);//UIColor;
+    }else if([key containsString:@"logBtnImgs"]){
+        NSArray* imgPaths = dict[key];
+        NSArray* logBtnImgs = [[NSArray alloc] initWithObjects:
+                               [UIImage imageNamed:imgPaths[0]],
+                               [UIImage imageNamed:imgPaths[1]],
+                               [UIImage imageNamed:imgPaths[2]],
+                               nil];
+        jvUIConfig.logBtnImgs = logBtnImgs;
+    }else if([key containsString:@"numberColor"]){
+        jvUIConfig.numberColor = UIColorFromRGBValue([dict[key] intValue]);
+    }else if([key containsString:@"numberSize"]){
+        jvUIConfig.numberSize = [dict[key] floatValue];//CGFloat;
+    }else if([key containsString:@"numFieldOffsetY"]){
+        jvUIConfig.numFieldOffsetY =  [dict[key] floatValue];//CGFloat;
+    }else if([key containsString:@"uncheckedImg"]){
+        jvUIConfig.uncheckedImg = [UIImage imageNamed:dict[key]];
+    }else if([key containsString:@"checkedImg"]){
+        jvUIConfig.checkedImg = [UIImage imageNamed:dict[key]];
+    }else if([key containsString:@"appPrivacyOne"]){
+        jvUIConfig.appPrivacyOne = dict[key];
+    }else if([key containsString:@"appPrivacyTwo"]){
+        jvUIConfig.appPrivacyTwo = dict[key];
+    }else if([key containsString:@"appPrivacyColor"]){
+        NSArray* colors = dict[key];
+        NSArray* appPrivacyColor = [[NSArray alloc] initWithObjects:
+                                    UIColorFromRGBValue([colors[0] intValue]),
+                                    UIColorFromRGBValue([colors[1] intValue]),
+                                    nil];
+        jvUIConfig.appPrivacyColor = appPrivacyColor;
+    }else if([key containsString:@"privacyState"]){
+        jvUIConfig.privacyState = [dict[key] boolValue];
+    }else if([key containsString:@"privacyOffsetY"]){
+        jvUIConfig.privacyOffsetY = [dict[key] floatValue];
+    }else if([key containsString:@"sloganOffsetY"]){
+        jvUIConfig.sloganOffsetY = [dict[key] floatValue];
+    }else if([key containsString:@"sloganTextColor"]){
+        jvUIConfig.sloganTextColor = UIColorFromRGBValue([dict[key] intValue]);
     }
     
 }
